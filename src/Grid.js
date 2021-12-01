90718175348b65f3579a6ecb9022ce3470272435
hofitA <hofit.adady@gmail.com>
2021-12-01 23:36:48 +0200

    Added solution

--- src/Grid.js
+++ src/Grid.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const Grid = ({ config, data }) => (
-  <table>
+const Grid = ({ config, data }) => {
+  return (<table>
     <thead>
-    <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
-    </tr>
+      <tr>
+        {config.map((col) => (
+          <th key={col.title}>{col.title}</th>
+        ))}
+      </tr>
     </thead>
-    <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
+    <tbody>      
+        {data.map((row, i) => {
+          return(
+            <tr key = {i}>
+              {config.map((col, j)=> {               
+                if(Object.keys(col).includes("component")) {
+                  return <td key = {j}>
+                    <col.component data= {row[config[j].field]} />
+                  </td>                  
+                }else {
+                  return <td key = {j}>{row[config[j].field]}</td>
+                }
+                })}
+            </tr> 
+          )
+        })}      
     </tbody>
   </table>
-);
+  )};
 
 export default Grid;
